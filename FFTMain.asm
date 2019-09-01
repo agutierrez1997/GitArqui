438bf2ca0b591f1ffaeff13c3901536e83122309
Allan Gutiérrez <allanguti@hotmail.com>
2019-09-01 15:33:51 -0600

    Primer commit
    Tiene portada

---
+++ FFTMain.asm
@@ -0,0 +1,11 @@
+; Instituto Tecnológico de Costa Rica
+; Escuela de Ingeniería Electrónica 
+; Arquitectura de Computadoras
+; II Semestre 2019
+; Proyecto 1: FFT
+; Osvaldo Alfaro Gonzáles
+; Alejandro Flores Herrera
+; Allan Gutiérrez Quesada
+; Eduardo Salazar Villalobos
+
+
