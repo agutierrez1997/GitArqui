75e5ed514ef69dde970861bb03269ae7c56f890d
Alejandro Flores <48072247+afloher97@users.noreply.github.com>
2019-10-03 11:27:34 -0600

    Update DFT.m

--- DFT.m
+++ DFT.m
@@ -1,4 +1,4 @@
-function [Wre,Wim] = DFT(N) %x vector columna
+function [Wre,Wim] = DFT(N) %N es el numero de puntos
 Wre=[];
 Wim=[];
 for k=0:N-1
@@ -7,6 +7,6 @@ for k=0:N-1
         Wim(k+1,n+1)=imag(exp(-1i*2*pi*k*n/N));
     end
 end
-Wre
+Wre %esto se pone para imprimir Wre y Wim
 Wim
 end
