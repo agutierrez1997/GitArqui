3574f76eb1a29777cb6b9eb3b94fdea04c961ae0
Alejandro Flores <afloher97@gmail.com>
2019-11-25 19:53:31 -0600

    Cambios en la salida

--- LFUComparador.sv
+++ LFUComparador.sv
@@ -7,7 +7,7 @@ module LFUComparador #(parameter sizeCounter = 4)(
     input logic [sizeCounter-1:0]       count2,
     input logic [sizeCounter-1:0]       count3,
     
-    output logic [1:0]                  cache_sel
+    output logic [3:0]                  cache_sel
     
     );
     
@@ -23,10 +23,10 @@ module LFUComparador #(parameter sizeCounter = 4)(
         
         if (menor>=count3) menor = count3;
         
-        if (menor==count0) cache_sel = 0;
-        if (menor==count1) cache_sel = 1;
-        if (menor==count2) cache_sel = 2;
-        if (menor==count3) cache_sel = 3;
+        if (menor==count0) cache_sel = 4'b0001;
+        if (menor==count1) cache_sel = 4'b0010;
+        if (menor==count2) cache_sel = 4'b0100;
+        if (menor==count3) cache_sel = 4'b1000;
     
     end
     
